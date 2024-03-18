39c008d1eb7cfe02492472b86bf458cf1e06fb9d
C4RBON73 <75441296+C4RBON73@users.noreply.github.com>
2024-03-18 15:34:02 +0100

    Appel Voiture Fonctionnnel

--- README.txt
+++ README.txt
@@ -1,2 +1,3 @@
 Pour lancer la page web : npm run start (à la racine du projet)
 Pour lancer le SOAP : Lancer le fichier CalculTempsTrajet_SOAP.py 
+
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
 var express = require('express');
 var router = express.Router();
+
 const path = require("path");
 const clientS = require("./ClientSOAP.js");
+const vehicule = require("./VehiculeQuery.js");
 
 
 /* GET home page. */
@@ -23,5 +25,18 @@ tpsRecharge= 0.15
         res.status(500).send('Une erreur est survenue côté serveur.');
     }
 });
+router.get('/cars', async (req, res) => {
+  try {
+      const result = await vehicule.carsQuery();
+      const resData = await result.json;
+      console.log(resData);
+      res.send(resData);
+  } catch (error) {
+      console.error('Une erreur est survenue :', error);
+      res.status(500).send('Une erreur est survenue côté serveur.');
+  }
+});
+
+
 
 module.exports = router;
